ce0a1dea1f899fb1aed33baeb640eb0a7fbe5009
RAJESH YADAV <rajesh_zero@outlook.com>
2021-12-20 22:44:20 +0530

    added colors and changed font

--- secondleft.js
+++ secondleft.js
@@ -1,8 +1,4 @@
-console.log("hi there2");
-
-// function dS(){
-//     console.log(86400 - seconds)
-// }
+var totalSeconds = 86400;
 function Display() {
 
 
@@ -13,11 +9,49 @@ function Display() {
         var sec = dts.getSeconds();
         var hradd = hr * 3600;
         var seconds = hradd + min + sec;
-        const a = 86400 - seconds;
+        const a = totalSeconds - seconds;
         let prin = document.getElementById('demo');
         prin.innerHTML = a;
+        prin.style.color = getColor(seconds);
     }, 1000);
 
 }
 
 Display();
+
+
+function getColor(seconds) {
+    if (seconds < 3600) {
+        return 'red';
+    } else if (seconds < 7200) {
+        return 'orange';
+    } else if (seconds < 10800) {
+        return 'yellow';
+    } else if (seconds < 14400) {
+        return 'green';
+    } else if (seconds < 18000) {
+        return 'blue';
+    } else if (seconds < 21600) {
+        return 'indigo';
+    } else if (seconds < 25200) {
+        return 'violet';
+    } else if (seconds < 28800) {
+        return 'purple';
+    } else if (seconds < 32400) {
+        return 'pink';
+    } else if (seconds < 36000) {
+        return 'brown';
+    } else if (seconds < 39600) {
+        return 'black';
+    } else if (seconds < 43200) {
+        return 'white';
+    } else if (seconds < 46800) {
+        return 'gray';
+    } else if (seconds < 50400) {
+        return 'silver';
+    } else if (seconds < 54000) {
+        return 'gold';
+    } else {
+        return 'red';
+    }
+}
